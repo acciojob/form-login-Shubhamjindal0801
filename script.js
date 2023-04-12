d680ed15be7f044d788fd74f5a97b1db1ff5e613
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-12 12:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
-    //Write your code here
-
+	const first = document.getElementById("first").value;
+    const last = document.getElementById("last").value;
+    alert(`${first} ${last}`)
 }
